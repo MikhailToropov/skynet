f41cc0770f3730c0c9e3e45176b12406dba5c20d
Mikhail Toropov <mihail.toropov@gmail.com>
2020-03-01 15:32:50 +0300

    init

---
+++ src/main/java/org/norns/App.java
@@ -0,0 +1,13 @@
+package org.norns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
