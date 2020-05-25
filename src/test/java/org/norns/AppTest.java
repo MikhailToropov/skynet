772b91b916a1bc89e4a4e35cfce07ebb158de9e4
Mikhail Toropov <mihail.toropov@gmail.com>
2020-05-25 21:32:01 +0300

    Back

--- src/test/java/org/norns/AppTest.java
+++ src/test/java/org/norns/AppTest.java
@@ -3,9 +3,12 @@ package org.norns;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 /**
  * Unit test for simple App.
@@ -27,6 +30,14 @@ public class AppTest
         Message sendMessage = new Message();
         Message incomMessage = new Message();
         assertTrue(true);
-
-    }
+        ApiContextInitializer.init();
+        TelegramBotsApi telegram = new TelegramBotsApi();
+        InfoBot bot = new InfoBot();
+        try{
+            telegram.registerBot(bot);
+        }
+        catch (TelegramApiRequestException e){
+            System.out.println(e);
+        }
+        }
 }
